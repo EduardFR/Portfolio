0923f747f6781d9933599c1f52d3214b05ce4461
EduardFR <118263500+EduardFR@users.noreply.github.com>
2023-01-20 16:09:54 +0400

    added project Multi-step-form

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,4 +1,5 @@
 import ProjectScrin from "../assets/ProjectScrin.jpeg";
+import ProjectMultiStepForm from "../assets/ProjectMultiStepForm.jpg";
 
 export const projects = [
   {
@@ -7,6 +8,31 @@ export const projects = [
     name: "KODE treinee test",
     linkToCode: "https://github.com/EduardFR/appKode.git",
     linkToProject: "https://app-kode.vercel.app",
+    skills: [
+      {
+        id: "1",
+        skill: "HTML",
+      },
+      {
+        id: "2",
+        skill: "Styled-Components",
+      },
+      {
+        id: "3",
+        skill: "React",
+      },
+      {
+        id: "4",
+        skill: "Redux",
+      },
+    ],
+  },
+  {
+    ID: "2",
+    url: `${ProjectMultiStepForm}`,
+    name: "Multi step form",
+    linkToCode: "https://github.com/EduardFR/multi-step-form.git",
+    linkToProject: "https://multi-step-form-brown-tau.vercel.app",
     skills: [
       {
         id: "1",
